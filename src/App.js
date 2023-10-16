d33e1c5a40b30fe0c3a47b8733118b96764b00d2
nageshwer tailor <nageshwer.tailor@qfixinfo.com>
2023-10-16 18:12:03 +0530

    code changes for  value

--- src/App.js
+++ src/App.js
@@ -4,32 +4,43 @@ import { Fragment } from 'react';
 import {dataSet} from "./utils/WineDataset"
 
 function App() {
- // use set for unique class 
-	let uniqueClasses = [...new Set(dataSet().map((entry) => entry.Class))];
+ // use set for  store unique class  Alchohal property  
+
+	let uniqueClasses = [...new Set(dataSet().map((entry) => entry.Alcohol))];
+  // uniqueClasses= 1,2,3 
  
     function calculateStatistics(){
       let statisticsByClass = {};
       uniqueClasses.forEach((className) => {
         let flavanoidsData = dataSet()
-          .filter((entry) => entry.Class === className)
+          .filter((entry) => entry.Alcohol === className)
           .map((entry) => parseFloat(entry.Flavanoids));
+          // in above code line 15,16,17 check data in wine date set json and set  uniqueClasses and  return in array
+          
+          // and  pass to the below  mean ,mediann and mode function
 
+          
+        
           //  call function for mean ,median and mode
         let mean = calculateMean(flavanoidsData);
         let median = calculateMedian(flavanoidsData);
         let mode = calculateMode(flavanoidsData);
         statisticsByClass[className] = { mean, median, mode };
       });
+      // return result mean , median or mode  in object 
       return statisticsByClass
     };
   
     function calculateMean(data){
+
+      // find sum and divide by length and fixed result fixed value two decimal
       let sum = data.reduce((acc, value) => acc + value, 0);
      
       return (sum / data.length).toFixed(2);
     };
   
     function calculateMedian(data){
+      // calculat and return median
       data.sort((a, b) => a - b);
       let middle = Math.floor(data.length / 2);
       if (data.length % 2 === 0) {
@@ -40,6 +51,7 @@ function App() {
     };
   
     function calculateMode(data){
+ // calculat and return mode
       const counts = {};
       let mode = null;
       let maxCount = 0;
@@ -69,23 +81,22 @@ function App() {
             <tr>
               <td style={{border:'1px solid black',padding:'5px 5px'}}>Flavanoids Mean</td>
              
-                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.mean}</td>
-                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.mean}</td>
-                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.mean}</td>
+                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[1].mean}</td>
+                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[2].mean}</td>
+                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[3].mean}</td>
           
             </tr>
             <tr>
               <td style={{border:'1px solid black',padding:'5px 5px'}}>Flavanoids Median</td>
-                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.median}</td>
-                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.median}</td>
-                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.median}</td>
+                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[1].median}</td>
+                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[2].median}</td>
+                  <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[3].median}</td>
             </tr>
             <tr>
               <td style={{border:'1px solid black',padding:'5px 5px'}}>Flavanoids Mode</td>
-              
-                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.mode}</td>
-                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.mode}</td>
-                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics().undefined.mode}</td>
+                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[1].mode}</td>
+                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[2].mode}</td>
+                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateStatistics()[3].mode}</td>
               
             </tr>
           </table>
@@ -97,6 +108,7 @@ function App() {
 
     // for calculate  gamma statistics :::
     function calculateGamma(){
+      // set the gamma value according to  given formula and save in to  existing object 
         let updatedDataset = dataSet().map((entry) => {
         let ash = parseFloat(entry.Ash);
         let hue = parseFloat(entry.Hue);
@@ -104,18 +116,19 @@ function App() {
         let gamma = (ash * hue) / magnesium;
         return { ...entry, Gamma: gamma };
       });
-  
       return updatedDataset;
     }
     function calculateGammaStats(){
       let updatedDataset = calculateGamma();
-      let uniqueClasses = [...new Set(updatedDataset.map((entry) => entry.Class))];
+      // save  in set by alcohal property for unique
+      let uniqueClasses = [...new Set(updatedDataset.map((entry) => entry.Alcohol))];
       let statisticsByClass = {};
 
       uniqueClasses.forEach((className) => {
         let gammaData = updatedDataset
-          .filter((entry) => entry.Class === className)
+          .filter((entry) => entry.Alcohol === className)
           .map((entry) => entry.Gamma);
+         // in above code find the date of gamma property and pass to function mean ,median, mode
           // claculate mean,median ,mode
         let mean = calculateMean(gammaData);
         let median = calculateMedian(gammaData);
@@ -136,26 +149,23 @@ function App() {
           </tr>
           <tr>
             <td style={{border:'1px solid black',padding:'5px 5px'}}>Gamma Mean</td>
-          
-            <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.mean}</td>
-            <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.mean}</td>
-            <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.mean}</td>
-           
+                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[1].mean}</td>
+                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[2].mean}</td>
+                <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[3].mean}</td>
+              
           </tr>
           <tr>
             <td style={{border:'1px solid black',padding:'5px 5px'}}>Gamma Median</td>
-             <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.median}</td>
-             <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.median}</td>
-             <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.median}</td>
+             <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[1].median}</td>
+             <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[2].median}</td>
+             <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[3].median}</td>
           
           </tr>
           <tr>
             <td style={{border:'1px solid black',padding:'5px 5px'}}>Gamma Mode</td>
-           
-              <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.mode}</td>
-              <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.mode}</td>
-              <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats().undefined.mode}</td>
-           
+              <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[1].mode}</td>
+              <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[2].mode}</td>
+              <td style={{border:'1px solid black',padding:'5px 5px'}}>{calculateGammaStats()[3].mode}</td>
           </tr>
         </table>
     )
